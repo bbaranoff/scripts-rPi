ec157e94c7cc558fa8e5f6154a8e04252d91bcd7
Bastien Baranoff <bastienbaranoff@gmail.com>
2021-04-16 10:52:05 +0200

    commented

--- abisip-ip.sh
+++ abisip-ip.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+#on test si le fichier monip est présent (initialisation)
 test -s monip
+#Tant que mon est vide on fait :
 while [[ $(echo $?) -eq 1 ]]; do
+#On lance abisip-find en tant que fork et
+#on prend le quatrième champ avec le séparateur ' on le stocke dans mon ip
+#on kill le fork après 3 secondes
 echo $(abisip-find | awk -F\' '{print $4}') > monip & (sleep 3 && killall -9 abisip-find)
+#on rexecute le test pour la boucle while
 test -s monip
 done
+#enfin on élimine les doublons et on stocke l'ip de la nanoBTS dans le fichier ip_nano
 sort -u monip > ip_nano
--- set_ip_nano.sh
+++ set_ip_nano.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-test -s ip_nano
-if [[ $? -eq 1 ]] ; then
+#on prend l'ip locale du pi
 ifconfig eth0 | awk '/inet / {print $2}' | cut -d ':' -f2 > ip_loc
+#On verifie que l'on a pas déja l ip de la nano
+test -s ip_nano
+#Si elle n'est pas dans un fichier c est que la nano n a pas été initialisée
+if [[ $? -eq 1 ]] ; then
+#on lance le script joint
 ./abisip-ip.sh > ip_nano
+#et enfin on configure la nano avec les ip recherchées
 ipaccess-config -r $(cat ip_nano) -o $(cat ip_loc) -u 1801/0/0
 fi
